fabbfd7c6c47e999275b085950576d09d9a5002c
malackapite <veress.marcell@protonmail.com>
2024-05-26 19:09:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Win32;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> AchWon = [
+            "Acidic Atoll",
+"Aftershock Arena",
+"Altitude Attack",
+"Animal Arithmetic",
+"Barn Brawl",
+"Battery Batters",
+"Billiard Battle",
+"Bouncing Balls",
+"Bounding Blocks",
+"Breaking Blocks",
+"Bullet Barrage",
+"Cannon Circle",
+"Crown Capture",
+"Daring Dogfight",
+"Elemental Escalation",
+"Explosive Exchange",
+"Foggy Fall",
+"Forklift Fling",
+"Fractured Faces",
+"Gift Grab",
+"Grifting Gifts",
+"House Hunting",
+"Laser Leap",
+"Magma & Mages",
+"Memory Menu",
+"Miniature Motors",
+"Morphing Maze",
+"Mortar Mayhem",
+"Motor Murder",
+"Mystery Maze",
+"Pack And Pile",
+"Pummel Punch",
+"Risky Rubberbands",
+"Rockin Rithm",
+"Rolly Ragdolls",
+"Rotor Race",
+"Rusty Racers",
+"Sandy Search",
+"Searing Spotlights",
+"Selfish Stride",
+"Sharky Swim",
+"Sidestep Slope",
+"Slippery Sprint",
+"Snowy Spin",
+"Sorcerers Sprint",
+"Speedy Sabers",
+"Spooky Spikes",
+"Stategic Shockwave",
+"Swift Shooters",
+"Temporal Trails",
+"Thunderous Trench",
+"Traffic Theft",
+"Tunneling Tanks",
+"Word Wars"
+            ];
+        List<string> AchLost = [
+            .. AchWon
+            ];
+        string keyPath = @"SOFTWARE\Rebuilt Games\Pummel Party";
+        using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath))
+        {
+            if (key != null)
+            {
+                foreach (string valueName in key.GetValueNames())
+                {
+                    if (valueName.IndexOf("_ACH_WON_") != -1)
+                        AchWon.Remove(valueName.Split('_')[0]);
+                    else if(valueName.IndexOf("_ACH_LOST_") != -1)
+                        AchLost.Remove(valueName.Split('_')[0]);
+                }
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Missing Ach won: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                foreach (var item in AchWon)
+                {
+                    Console.WriteLine("\t" + item);
+                }
+                Console.ForegroundColor= ConsoleColor.DarkRed;
+                Console.WriteLine();
+                Console.WriteLine("Missing Ach lost: ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                foreach (var item in AchLost)
+                {
+                    Console.WriteLine("\t"+item);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Cannot find key: {keyPath}");
+            }
+        }
+    }
+}
